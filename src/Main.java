dd1f20dfb963fa163fdde39b2dff197fe636c27f
MarselMingaripov <bard1812@gmail.com>
2022-12-09 16:31:45 +0300

    выполнил задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
+    }
+    public static void task1(){
+        System.out.println("task 1");
+        double total = 0;
+        int month = 0;
+        while (total <= 2459000){
+            total += 15000;
+            month++;
+        }
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        System.out.println();
     }
 }
